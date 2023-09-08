af35371d009c34a4239320fbbc11dc232cba3cf7
Krister Sigvaldsen Moen <krister@beiningbogen.no>
2023-09-08 22:15:44 +0200

    Initial Commit

---
+++ oddvar/oddvarApp.swift
@@ -0,0 +1,17 @@
+//
+//  oddvarApp.swift
+//  oddvar
+//
+//  Created by Krister Sigvaldsen Moen on 08/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct oddvarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
