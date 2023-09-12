a5d48282257b255299d30c8bbeb130ff40e77552
Krister Sigvaldsen Moen <krister@beiningbogen.no>
2023-09-12 22:07:45 +0200

    remove old framework and some unused code

--- oddvar/OddvarState.swift
+++ oddvar/OddvarState.swift
@@ -51,7 +51,6 @@ public class OddvarState: ObservableObject {
         _ = try await task.value
     }
     
-    @discardableResult
     public func deleteAll() async throws {
         let task = Task {
             let outfile = try Self.fileURL()
